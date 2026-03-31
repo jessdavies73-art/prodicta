a0aec7d6516dc11528c1605e5610d662c09baf42
Jessie <jr@aiaura.co.uk>
2026-03-31 13:22:43 +0100

    Fix Prodicta wordmark colour to navy on login page

--- app/login/page.js
+++ app/login/page.js
@@ -227,7 +227,7 @@ function Logo() {
         <span style={{
           fontSize: '28px',
           fontWeight: '800',
-          color: '#ffffff',
+          color: '#0f2137',
           letterSpacing: '-0.5px',
           lineHeight: 1,
           fontFamily: "'Outfit', system-ui, sans-serif",
